ec462dc7b18596e342d94b634a68edeba0b65dda
unknown <forkan.saadaoui@gmail.com>
2023-09-25 19:48:43 +0100

    connect to db

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -16,19 +17,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// routes
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-/**
- * Errors handling
- */
-app.use((err, req, res, next) => {
-    if(err.name === 'MongoError' || err.name === 'ValidationError' || err.name === 'CastError'){
-        err.status = 422;
-    }
-    if(req.get('accept').includes('json')){
-        res.status(err.status || 500).json({message: err.message || 'some error eccured.'});
-    } else {
-        res.status(err.status || 500).sendFile(path.join(__dirname, 'public', 'index.html'));
-    }
+// connect to db
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useCreateIndex: true}, err =>  {
+    if(err) throw err;
+    console.log('Connected successfully');
 });
 
 
